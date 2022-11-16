c54a6440635b0c57a35fd8a8122b4df63756e59b
VFS\pg23mercedes <pg23mercedes@vfs.com>
2022-11-16 12:42:02 -0800

    commit 5

--- scripts/App.js
+++ scripts/App.js
@@ -5,8 +5,38 @@ export default class App {
     constructor(){
 
         $("#save-btn").on("click", event => this.SaveForm(event));
+
+        $("#level-list").on("click", event => this.GetLevelList());
     }
 
+    
+    SaveForm(event) {
+        event.preventDefault();
+        
+        let id = "pg23Mercedes"
+        let entities = {};
+        let name = $("#name").value;
+                
+        entities = this.GetObjectAttr();
+        
+        //level data to save
+        let data = {
+            "id": id,
+            "name": name,
+            "payload": {
+                "name": name,
+                "maxShots": $("#shots").value,
+                "oneStarScore": $("#star1").value,
+                "twoStarScore": $("#star2").value,
+                "threeStarScore": $("#star3").value,
+                "entities": entities
+            }
+            
+        }
+        
+        this.SaveData('/api/post', data);
+    }
+    
     GetObjectAttr() {
 
         const gameArea = $("#edit-window");
@@ -27,46 +57,35 @@ export default class App {
 
         return gameObjectsArray;
     }
-
-    SaveForm(event) {
-        event.preventDefault();
-
-        const levelData = $("#level-info").serialize();
-
-        let entities = this.GetObjectAttr();
-
-        //level data to save
-        let data = {
-            id: "Level",
-            name: $("#name").value,
-            maxShots: $("#shots").value,
-            oneStarScore: $("#star1").value,
-            twoStarScore: $("#star2").value,
-            threeStarScore: $("#star3").value,
-            entities: entities,
-            payload: levelData
-
-        }
-
-        fetch("/api/save", {
-            method: "POST",
+    
+    //code from https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+    async SaveData(url ='/api/post', req_data = {}) {
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors', 
+            cache: 'no-cache', 
+            credentials: 'same-origin',
             headers: {
-                "Content-Type": "application/json",
+            'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data),
+            redirect: 'follow', 
+            referrerPolicy: 'no-referrer', 
+            body: JSON.stringify(req_data) 
         });
+        return response.json(); // parses JSON response into native JavaScript objects
     }
 
-    //Scotts on save
-    onSave( event ) {
-        event.preventDefault();
-        let level = new LevelController( this.editor$ );
-        level.save()
-            .then( response => {
-                alert(`Level saved`)
-            })
-            .catch( response => {
-                alert('Level not saved')
-            })
+    GetLevelList(){
+        $.post("/api/get_level_list").then(data => {  
+            var namesDataList = $("#names"); 
+            let name = $("#name").value;
+            data = data.payload; 
+           
+            data.forEach(function (file) {
+                var option = document.createElement('option');
+                option.value = file.name; 
+                namesDataList.appendChild(option);
+            });
+        })
     }
 }
